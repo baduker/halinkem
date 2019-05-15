d1fc0e7dcbdffc84ff165805f2a8f3f784f4fc8e
baduker <baduker@protonmail.ch>
2019-05-15 20:55:23 +0200

    Version 0.2 completed and working!

--- halinkem.py
+++ halinkem.py
@@ -4,22 +4,24 @@ import time
 import os
 import sys
 import requests
+
+
 from bs4 import BeautifulSoup as bs
+
 
 DEFAULT_DIR = "my_halinki"
 COMICS_DIR = os.path.join(os.getcwd(), DEFAULT_DIR)
 BASE_URL = "http://www.pani-halinka.pl/main/"
 
+
 LOGO = """
- __ __   ____  _      ____  ____   __  _ __    ___  ___ ___       ____  _      _      __ 
-|  |  | /    || |    |    ||    \ |  |/ ]  |  /  _]|   |   |     /    || |    | |    |  |
-|  |  ||  o  || |     |  | |  _  ||  ' /|_ | /  [_ | _   _ |    |  o  || |    | |    |  |
-|  _  ||     || |___  |  | |  |  ||    \  \||    _]|  \_/  |    |     || |___ | |___ |__|
-|  |  ||  _  ||     | |  | |  |  ||     \   |   [_ |   |   |    |  _  ||     ||     | __ 
-|  |  ||  |  ||     | |  | |  |  ||  .  |   |     ||   |   |    |  |  ||     ||     ||  |
-|__|__||__|__||_____||____||__|__||__|\_|   |_____||___|___|    |__|__||_____||_____||__|
-                                                                                         
-  a comic scraper for tajo by baduker | version: 0.1 | repo: github.com/baduker/halinkem
+██╗  ██╗ █████╗ ██╗     ██╗███╗   ██╗██╗  ██╗███████╗███╗   ███╗
+██║  ██║██╔══██╗██║     ██║████╗  ██║██║ ██╔╝██╔════╝████╗ ████║
+███████║███████║██║     ██║██╔██╗ ██║█████╔╝ █████╗  ██╔████╔██║
+██╔══██║██╔══██║██║     ██║██║╚██╗██║██╔═██╗ ██╔══╝  ██║╚██╔╝██║
+██║  ██║██║  ██║███████╗██║██║ ╚████║██║  ██╗███████╗██║ ╚═╝ ██║
+╚═╝  ╚═╝╚═╝  ╚═╝╚══════╝╚═╝╚═╝  ╚═══╝╚═╝  ╚═╝╚══════╝╚═╝     ╚═╝'em all!                                                                                      
+a comic scraper for tajo by baduker | v. 0.2 | github.com/baduker/halinkem
 """
 
 
@@ -60,6 +62,16 @@ def get_main_urls():
   return [BASE_URL+str(i)+"/" for i in range(1,32)]
 
 
+def get_all_comic_src_urls(main_urls):
+  counter = 0
+  all_src_urls = []
+  for page in range(len(main_urls)):
+    print_progress(counter, len(main_urls), prefix = 'Finding Halinki:', suffix = 'Compelted!', bar_length = 18)
+    response = requests.get(main_urls[page])
+    all_src_urls.append(get_image_comic_url(response))
+    counter += 1
+  return all_src_urls
+
 def flat_list_of_all_src_urls(nested_array):
   return [src_url for src_url_sublist in nested_array for src_url in src_url_sublist]
 
@@ -78,29 +90,22 @@ def main():
 
   main_urls = get_main_urls()
 
+  all_comic_src_urls = get_all_comic_src_urls(main_urls)
 
-  counter = 0
-  all_src_urls = []
-  for page in range(len(main_urls)):
-    print_progress(counter, len(main_urls), prefix = 'Finding Halinki', suffix = ' Compelte', bar_length = 18)
-    response = requests.get(main_urls[page])
-    all_src_urls.append(get_image_comic_url(response))
-    counter += 1
+  flat_list = flat_list_of_all_src_urls(all_comic_src_urls)
 
-  flat_list = flat_list_of_all_src_urls(all_src_urls)
-
-  print("\nDone!")
+  print("\nDone! All Halinki found.")
 
   os.makedirs(DEFAULT_DIR, exist_ok = True)
 
   counter = 0
   for i in flat_list:
-    print_progress(counter, len(flat_list), prefix = 'Downloading Halinki', suffix = 'Complete', bar_length = 18)
+    print_progress(counter, len(flat_list), prefix = 'Downloading Halinki:', suffix = 'Completed!', bar_length = 18)
     download_engine(i)
     counter += 1
 
   end = time.time()
-  print("\n{} Halinki downloaded in {:.2f} seconds".format(len(flat_list), end - start))
+  print("\nFinished! {} Halinki downloaded in {:.2f} seconds.".format(len(flat_list), end - start))
 
 
 if __name__ == '__main__':
